c9f7f7135c4495955a02dcc58ff1365ab2fdc094
didbre <didier.breton@gmail.com>
2022-02-03 22:08:54 -0500

    Début chapitre 6 Eureka Server
    fin section 6.3

--- eurekaserver/src/test/java/com/optimagrowth/eurekaserver/EurekaserverApplicationTests.java
+++ eurekaserver/src/test/java/com/optimagrowth/eurekaserver/EurekaserverApplicationTests.java
@@ -1,13 +1,13 @@
-package com.optimagrowth.license;
+package com.optimagrowth.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LicensingServiceApplicationTests {
+class EurekaserverApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
