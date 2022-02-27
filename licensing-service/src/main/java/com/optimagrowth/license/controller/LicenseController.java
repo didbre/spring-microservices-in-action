0f24c35b0b289b56efe0d5c76efefb5e495d3784
didbre <didier.breton@gmail.com>
2022-02-27 12:36:32 -0500

    < dockker-compose > Ajout d'un ordre de procession de fichier
    < LicenseController >
    Ajout d'une méthode getLicenses

--- licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -2,15 +2,21 @@ package com.optimagrowth.license.controller;
 
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.optimagrowth.license.utils.UserContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
+  private static final Logger logger = LoggerFactory.getLogger(LicenseController.class);
   final LicenseService licenseService;
 
   public LicenseController(LicenseService licenseService) {
@@ -20,7 +26,7 @@ public class LicenseController {
 //  example de requete
 //  http localhost:8080/v1/organization/d898a142-de44-466c-8c88-9ceb2c2429d3/license/f2a9c9d4-d2c0-44fa-97fe-724d77173c62/rest
   @GetMapping(value = "/{licenseId}/{clientType}")
-  public ResponseEntity<License> getLicense(
+  public ResponseEntity<License> getLicenseWithClient(
       @PathVariable("organizationId") String organizationId,
       @PathVariable("licenseId") String licenseId,
       @PathVariable("clientType") String clientType) {
@@ -51,5 +57,12 @@ public class LicenseController {
       @RequestHeader(value = "Accept-Language", required = false) Locale locale)
   {
     return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+  }
+
+  @RequestMapping(value="/",method = RequestMethod.GET)
+  public List<License> getLicenses(@PathVariable("organizationId") String organizationId) throws TimeoutException
+  {
+    logger.debug("LicenseServiceController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+    return licenseService.getLicensesByOrganization(organizationId);
   }
 }
