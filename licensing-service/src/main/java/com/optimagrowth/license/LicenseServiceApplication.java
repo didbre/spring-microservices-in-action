36f2af68cb37440bf2b7b6fa288320b44e2a3a20
Didier Breton <didier.breton@gmail.com>
2022-03-02 11:33:25 -0500

    fin chapitre 8

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,5 +1,7 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.utils.UserContextInterceptor;
+import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +10,13 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -41,7 +47,16 @@ public class LicenseServiceApplication
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+		if (interceptors == null) {
+			restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		}
+		else {
+			restTemplate.setInterceptors(interceptors);
+		}
+
+		return restTemplate;
 	}
 
 
